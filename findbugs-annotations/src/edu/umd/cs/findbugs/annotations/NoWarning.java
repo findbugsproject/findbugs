30af7c3734dac8960faf4f3f0f48ad11ad3f77f1
Andrey Loskutov <iloveeclipse@gmail.com>
2009-07-06 23:23:29 +0000

    Separated annotations to the project. Fixed annotations.jar building: added sources and made it OSGI bundle. Updated projects classpath. Updated central build.xml.
    git-svn-id: https://findbugs.googlecode.com/svn/branches/2.0_gui_rework@11400 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs-annotations/src/edu/umd/cs/findbugs/annotations/NoWarning.java
@@ -0,0 +1,35 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2008, University of Maryland
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Annotation indicating that <em>no</em> FindBugs warning is expected.
+ *
+ * @author David Hovemeyer
+ */
+@Retention(RetentionPolicy.CLASS)
+public @interface NoWarning {
+	// Comma-separated list of bug codes (e.g., "NP") not expected
+	// in the annotated method.
+	public String value();
+}
