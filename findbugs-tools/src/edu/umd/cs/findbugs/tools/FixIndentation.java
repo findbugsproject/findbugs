0f60936cbbb91726a8031b77f385cf8569b0e267
Andrey Loskutov <iloveeclipse@gmail.com>
2009-07-10 07:03:24 +0000

    Moved tools to the separate project
    git-svn-id: https://findbugs.googlecode.com/svn/branches/2.0_gui_rework@11412 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs-tools/src/edu/umd/cs/findbugs/tools/FixIndentation.java
+++ findbugs-tools/src/edu/umd/cs/findbugs/tools/FixIndentation.java
@@ -1,17 +1,17 @@
 /*
  * FindBugs - Find Bugs in Java programs
  * Copyright (C) 2006, University of Maryland
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -53,26 +53,33 @@ public class FixIndentation {
 		while (!todo.isEmpty()) {
 			File next = todo.remove().getAbsoluteFile();
 			String nextPath = next.getAbsolutePath();
-			if (!nextPath.startsWith(rootPath))
-				continue;
+			if (!nextPath.startsWith(rootPath)) {
+                continue;
+            }
 
 			if (next.isDirectory()) {
 				File[] contents = next.listFiles();
-				if (contents != null)
-					for (File c : contents)
-						if (seen.add(c))
-							todo.add(c);
-			} else if (nextPath.endsWith(".java"))
-				fix(next, partial);
+				if (contents != null) {
+                    for (File c : contents) {
+                        if (seen.add(c)) {
+                            todo.add(c);
+                        }
+                    }
+                }
+			} else if (nextPath.endsWith(".java")) {
+                fix(next, partial);
+            }
 		}
 	}
 
 	static String fix(String s) {
-		if (s.length() == 0)
-			return s;
-		if (s.trim().length() == 0) return "";
+		if (s.length() == 0) {
+            return s;
+        }
+		if (s.trim().length() == 0) {
+            return "";
+        }
 		int spaces = 0;
-		boolean seenTabs = false;
 		boolean badSpaces = false;
 		int pos = 0;
 		int indentation = 0;
@@ -83,11 +90,12 @@ public class FixIndentation {
 				spaces++;
 			} else if (c == '\t') {
 				indentation += 4;
-				seenTabs = true;
-				if (spaces > 0)
-					badSpaces = true;
-			} else
-				break;
+				if (spaces > 0) {
+                    badSpaces = true;
+                }
+			} else {
+                break;
+            }
 			pos++;
 		}
 		if (badSpaces || spaces > 1) {
@@ -108,31 +116,38 @@ public class FixIndentation {
 		try {
 			while (true) {
 				String s = in.readLine();
-				if (s == null)
-					break;
+				if (s == null) {
+                    break;
+                }
 				String s2 = fix(s);
 				if (!s2.equals(s)) {
 					consecutiveFixes++;
 					if (consecutiveFixes > 3 && partial) {
 						s2 = s;
 						consecutiveFixes = 0;
-					} else anyChanges = true;
-				} else
-					consecutiveFixes = 0;
+					} else {
+                        anyChanges = true;
+                    }
+				} else {
+                    consecutiveFixes = 0;
+                }
 				out.println(s2);
 			}
 		} finally {
 			in.close();
 		}
-		if (!anyChanges) return;
+		if (!anyChanges) {
+            return;
+        }
 		StringReader stringReader = new StringReader(stringWriter.toString());
 		FileWriter outFile = new FileWriter(fileToUpdate);
 		char[] buffer = new char[1000];
 		try {
 			while (true) {
 				int sz = stringReader.read(buffer);
-				if (sz < 0)
-					break;
+				if (sz < 0) {
+                    break;
+                }
 				outFile.write(buffer, 0, sz);
 			}
 		} finally {
