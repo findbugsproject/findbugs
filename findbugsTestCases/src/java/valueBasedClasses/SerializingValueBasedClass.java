c37eae996b518fbbffb202d0b3343b431b63237c
nicolaiparlog <nipa@codefx.org>
2014-12-05 21:08:01 +0100

    Create a test for a non-transient field of a value-based type in a serializable class.

---
+++ findbugsTestCases/src/java/valueBasedClasses/SerializingValueBasedClass.java
@@ -0,0 +1,15 @@
+package valueBasedClasses;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
+public class SerializingValueBasedClass implements Serializable{
+    
+    private static final long serialVersionUID = 0L;
+
+    @ExpectWarning("SE_BAD_FIELD")
+    private Optional<String> nonTransientFieldOptional;
+    
+}
