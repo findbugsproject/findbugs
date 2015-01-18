32353183df692d6aa16ecd52c51d8fc0f3d8f9c1
nicolaiparlog <nipa@codefx.org>
2015-01-18 11:51:11 +0100

    Add tests for writing value-based instances to 'ObjectOutputStream.writeObject'

--- findbugsTestCases/src/java/valueBasedClasses/SerializingValueBasedClass.java
+++ findbugsTestCases/src/java/valueBasedClasses/SerializingValueBasedClass.java
@@ -1,19 +1,31 @@
 package valueBasedClasses;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Optional;
 
 import edu.umd.cs.findbugs.annotations.ExpectWarning;
 
-@SuppressWarnings("unused")
-public class SerializingValueBasedClass implements Serializable{
-    
+public class SerializingValueBasedClass implements Serializable {
+
     private static final long serialVersionUID = 0L;
 
     @ExpectWarning("SE_BAD_FIELD")
     private Optional<String> nonTransientFieldOptional;
-    
+
     @ExpectWarning("SE_BAD_FIELD")
     private MyValueBasedClass nonTransientFieldValueBasedClass;
-    
+
+    public SerializingValueBasedClass() {
+        nonTransientFieldOptional = null;
+        nonTransientFieldValueBasedClass = null;
+    }
+
+    @ExpectWarning("DMI_NONSERIALIZABLE_OBJECT_WRITTEN ")
+    private void writeObject(ObjectOutputStream stream) throws IOException {
+        stream.writeObject(nonTransientFieldOptional);
+        stream.writeObject(nonTransientFieldValueBasedClass);
+    }
+
 }
